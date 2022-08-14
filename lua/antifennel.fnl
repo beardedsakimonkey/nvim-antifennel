e6bf6e2ac40b49e0acd45c4821dc7f96e53e6d37
beardedsakimonkey <sakimonkey@protonmail.com>
2022-08-14 15:41:35 -0700

    switch to io.popen

--- lua/antifennel.fnl
+++ lua/antifennel.fnl
@@ -1,13 +1,3 @@
-(local DEBUG false)
-
-(fn log [...]
-  (when DEBUG
-    (print "[nvim-antifennel] " ...)))
-
-(fn assert-exists [path]
-  (when DEBUG
-    (assert (vim.loop.fs_access path :R))))
-
 (fn create-file [filename text]
   (local handle (assert (io.open filename :w+)))
   (handle:write text)
@@ -18,45 +8,25 @@
   (local dirname (string.sub source 2 (* (string.len :/lua/antifennel.lua) -1)))
   (local sep (package.config:sub 1 1))
   (local path (.. dirname (.. :vendor sep :antifennel)))
-  (assert-exists path)
   path)
 
-(fn strip-trailing-newlines [str]
-  (var ret str)
-  ;; Loop because both stdout and antifennel include a trailing newline.
-  (while (= "\n" (ret:sub -1))
-    (set ret (ret:sub 1 -2)))
-  ret)
+(fn run-antifennel [filename _start-line _end-line]
+  (local cmd (.. (antifennel-script) " " filename))
+  (local fh (io.popen cmd))
+  (local lines {})
+  (each [line (fh:lines)]
+    (table.insert lines line))
+  (fh:close)
+  ;; Remove last line if it's empty
+  (when (= "" (. lines (length lines)))
+    (table.remove lines))
+  lines)
 
-(fn run-antifennel [filename start-line end-line]
-  (local stdout (vim.loop.new_pipe))
-  (local stderr (vim.loop.new_pipe))
-
-  (fn on-stdout [?err ?data]
-    ;; TODO: Need to handle buffered stdout
-    (log (: "on-stdout() with err %s, data %s" :format ?err ?data))
-    (when (not= nil ?data)
-      (local lines (vim.split (strip-trailing-newlines ?data) "\n"))
-      (vim.schedule (fn []
-                      ;; Delete existing lines
-                      (vim.api.nvim_buf_set_lines 0 start-line end-line true [])
-                      ;; Insert text at `start-line`
-                      (vim.api.nvim_buf_set_lines 0 start-line start-line true
-                                                  lines)))))
-
-  (fn on-stderr [?err ?data]
-    (log (: "on-stderr() with err %s, data %s" :format ?err ?data)))
-
-  (fn on-exit [code signal]
-    (log (: "on-exit() with exit code %d, signal %d" :format code signal))
-    (when (not= 0 code)
-      (log (: "spawn failed (exit code %d, signal %d)" :format code signal)))
-    (assert (os.remove filename)))
-
-  (vim.loop.spawn (antifennel-script)
-                  {:args [filename] :stdio [nil stdout stderr]} on-exit)
-  (vim.loop.read_start stdout on-stdout)
-  (vim.loop.read_start stderr on-stderr))
+(fn replace-lines [start-line end-line lines]
+  ;; Delete existing lines
+  (vim.api.nvim_buf_set_lines 0 start-line end-line true [])
+  ;; Insert text at `start-line`
+  (vim.api.nvim_buf_set_lines 0 start-line start-line true lines))
 
 ;; TODO: Support charwise selection. (can use nvim_buf_set_text?)
 (fn run [start-line end-line]
@@ -65,9 +35,9 @@
         input (vim.api.nvim_buf_get_lines 0 start-line end-line true)
         filename (vim.fn.tempname)]
     (create-file filename (table.concat input "\n"))
-    (run-antifennel filename start-line end-line)
-    ;; Careful adding code here; `run-antifennel` is async.
-    ))
+    (local lines (run-antifennel filename start-line end-line))
+    (replace-lines start-line end-line lines)
+    (os.remove filename)))
 
 {: run}
 
--- lua/antifennel.lua
+++ lua/antifennel.lua
@@ -1,75 +1,41 @@
-local DEBUG = false
-local function log(...)
-  if DEBUG then
-    return print("[nvim-antifennel] ", ...)
-  else
-    return nil
-  end
-end
-local function assert_exists(path)
-  if DEBUG then
-    return assert(vim.loop.fs_access(path, "R"))
-  else
-    return nil
-  end
-end
 local function create_file(filename, text)
   local handle = assert(io.open(filename, "w+"))
   handle:write(text)
   return handle:close()
 end
 local function antifennel_script()
-  local _local_3_ = debug.getinfo(1)
-  local source = _local_3_["source"]
+  local _local_1_ = debug.getinfo(1)
+  local source = _local_1_["source"]
   local dirname = string.sub(source, 2, (string.len("/lua/antifennel.lua") * -1))
   local sep = (package.config):sub(1, 1)
   local path = (dirname .. ("vendor" .. sep .. "antifennel"))
-  assert_exists(path)
   return path
 end
-local function strip_trailing_newlines(str)
-  local ret = str
-  while ("\n" == ret:sub(-1)) do
-    ret = ret:sub(1, -2)
+local function run_antifennel(filename, _start_line, _end_line)
+  local cmd = (antifennel_script() .. " " .. filename)
+  local fh = io.popen(cmd)
+  local lines = {}
+  for line in fh:lines() do
+    table.insert(lines, line)
   end
-  return ret
+  fh:close()
+  if ("" == lines[#lines]) then
+    table.remove(lines)
+  else
+  end
+  return lines
 end
-local function run_antifennel(filename, start_line, end_line)
-  local stdout = vim.loop.new_pipe()
-  local stderr = vim.loop.new_pipe()
-  local function on_stdout(_3ferr, _3fdata)
-    log(("on-stdout() with err %s, data %s"):format(_3ferr, _3fdata))
-    if (nil ~= _3fdata) then
-      local lines = vim.split(strip_trailing_newlines(_3fdata), "\n")
-      local function _4_()
-        vim.api.nvim_buf_set_lines(0, start_line, end_line, true, {})
-        return vim.api.nvim_buf_set_lines(0, start_line, start_line, true, lines)
-      end
-      return vim.schedule(_4_)
-    else
-      return nil
-    end
-  end
-  local function on_stderr(_3ferr, _3fdata)
-    return log(("on-stderr() with err %s, data %s"):format(_3ferr, _3fdata))
-  end
-  local function on_exit(code, signal)
-    log(("on-exit() with exit code %d, signal %d"):format(code, signal))
-    if (0 ~= code) then
-      log(("spawn failed (exit code %d, signal %d)"):format(code, signal))
-    else
-    end
-    return assert(os.remove(filename))
-  end
-  vim.loop.spawn(antifennel_script(), {args = {filename}, stdio = {nil, stdout, stderr}}, on_exit)
-  vim.loop.read_start(stdout, on_stdout)
-  return vim.loop.read_start(stderr, on_stderr)
+local function replace_lines(start_line, end_line, lines)
+  vim.api.nvim_buf_set_lines(0, start_line, end_line, true, {})
+  return vim.api.nvim_buf_set_lines(0, start_line, start_line, true, lines)
 end
 local function run(start_line, end_line)
   local start_line0 = (start_line - 1)
   local input = vim.api.nvim_buf_get_lines(0, start_line0, end_line, true)
   local filename = vim.fn.tempname()
   create_file(filename, table.concat(input, "\n"))
-  return run_antifennel(filename, start_line0, end_line)
+  local lines = run_antifennel(filename, start_line0, end_line)
+  replace_lines(start_line0, end_line, lines)
+  return os.remove(filename)
 end
 return {run = run}
